76d51eb0ebbf1d58ee00ce303f66c91749c20deb
Alejandro Chavez <alechavez93@gmail.com>
2016-01-24 05:01:56 -0500

    Fixed frame, and added aim function

--- script.js
+++ script.js
@@ -142,20 +142,14 @@ function TankShot(x,y,speed,damage,angle){
 	this.velocityX = Math.cos(angle)*speed;
 	this.velocityY = Math.sin(angle)*speed;
 	this.deltaAngle = 0;
-	this.initialAngle = this.angle;
-	this.firstRotation = true; 
 
 	//Functions
 	this.updateShot = function(){
 		this.x += this.velocityX;
 		this.y -= this.velocityY;
 		this.velocityY -= this.gravity;
-		console.log("current " + this.angle);
-		console.log("diff "	+ (Math.atan(this.velocityY/this.velocityX)));
 		this.deltaAngle = this.angle - (Math.atan(this.velocityY/this.velocityX));
 		this.angle -= this.deltaAngle;
-		// console.log(this.initialAngle);
-		// console.log(this.angle);
 	}
 
 	this.updateProportions = function(){
@@ -166,13 +160,7 @@ function TankShot(x,y,speed,damage,angle){
 	this.draw = function(){
 		ctx.save();
 		ctx.translate(this.x+this.width/2,this.y+this.height/2);
-		if (this.firstRotation) {
-			ctx.rotate(this.initialAngle);
-			this.firstRotation = false;
-		}
-		else {
 			ctx.rotate(-1 * this.angle);
-		}
 		ctx.drawImage(this.image[this.type], 0, 0, this.width, this.height);
 		ctx.restore();
 	}
@@ -223,6 +211,11 @@ function Tank(x,y,color,stance){
 		}
 	}
 
+	//Updates the cannon aim
+	this.updateAim = function(){
+
+	}
+
 
 	//Regular shot
 	this.shoot = function(){
@@ -231,12 +224,13 @@ function Tank(x,y,color,stance){
 
 	//Missile shot
 	this.shootMissile = function(){
-
+		//In development
+		//LOL
 	} 
 
 	//Aim weapons
 	this.aim = function(angle){
-
+		this.shotAngle = angle;
 	}
 
 	//Loads the spesific image combo
