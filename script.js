a98150aa55fb4cb32660a2d2ffc8bde5c7a45290
Alejandro Chavez <alechavez93@gmail.com>
2016-01-24 01:49:07 -0500

    update

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ var gameRunning = true;
 var canvas = document.getElementById("gameCanvas")
 var ctx = canvas.getContext("2d");
 var tankSpeed = 3;
-
+var tankShotDamage = 25
+var tankShotSpeed = 50;
 
 
 
@@ -20,8 +21,13 @@ var t2 = new Tank(1100, 350, "blue", "left");
 t.loadGraphics();
 t2.loadGraphics();
 
+ctx.drawImage(t.images[t.type], 0, 0, 50, 50);
 
-var gameLoop = setInterval(game, 15);
+// t.tankShot.draw();
+
+
+
+// var gameLoop = setInterval(game, 15);
 
 
 
@@ -32,7 +38,7 @@ var gameLoop = setInterval(game, 15);
 
 //Input for the tank 1 (left(A) and right(D))
 $(document).ready(function(){
-	$('canvas').css('display', 'none');
+	// $('canvas').css('display', 'none');
 	//ON CLICK
 	$(document).keydown(function(e){
 		//Left P1
@@ -126,15 +132,18 @@ function Missile(speed,damage){
 function TankShot(x,y,speed,damage,angle){
 	
 	//Fields
-	this.x = x;
-	this.y = y;
+	this.x = 0;
+	this.y = 0;
+	this.width = 150;
+	this.height = 150;
 	this.angle = angle;
 	this.speed = speed;
 	this.damage = damage;
 	this.gravity = -1.1;
 
 	//Complex properties
-	this.image;
+	this.type = 0;
+	this.image = document.getElementsByClassName("shot");
 	this.velocityX = Math.cos(angle);
 	this.velocityY = Math.sin(angle);
 
@@ -146,9 +155,22 @@ function TankShot(x,y,speed,damage,angle){
 		this.angle = Math.atan(this.y/this.x);
 	}
 
+	this.updateProportions = function(){
+		this.width =  this.image[this.type].width/8;
+		this.height =  this.image[this.type].height/8;
+	}
+
 	this.draw = function(){
-		ctx.rotate(angle);
-		ctx.drawImage()
+		// ctx.rotate(this.angle);
+		console.log(this.x);
+		console.log(this.y);
+		console.log(this.height);
+		console.log(this.width);
+		console.log(this.images[this.type]);
+		ctx.drawImage(this.images[this.type], 0, 0, 50, 50);
+
+
+		//ctx.drawImage(this.images[this.type], this.x, this.y, this.width, this.height);
 	}
 }
 
@@ -166,16 +188,17 @@ function Tank(x,y,color,stance){
 	this.stance = stance;
 
 	//Complex properties
+	this.shooting = true;
 	this.tankLeft = false;
 	this.tankRight = false;
 	this.factor = 4;
 	this.HP = 100;
+	this.shotAngle = 0.8;
+	this.tankShot = new TankShot(this.x, this.y, tankShotSpeed, tankShotDamage, this.shotAngle);
 	this.weaponX;		//int
 	this.weaponY;		//int
-	this.tankShot;		//object
 	this.missile;		//object
 	this.faceAlngle;	//int [0,360)
-	this.shotAngle;		//int [0,360)
 	this.missileAmmo;	//int [0,3]
 	this.image;			//png.file
 	this.type;			//int
@@ -199,7 +222,7 @@ function Tank(x,y,color,stance){
 
 	//Regular shot
 	this.shoot = function(){
-
+		this.shooting = true;
 	}
 
 	//Missile shot
@@ -219,21 +242,27 @@ function Tank(x,y,color,stance){
 		if(this.color == "blue"){
 			this.type = 0;
 			//Initialize the graphics for the weapons
+			this.tankShot.type = 0;
 		}
 		else if(this.color == "green"){
 			this.type = 1;
 			//Initialize the graphics for the weapons
+			this.tankShot.type = 1;
 		}
 		else if(this.color == "pink"){
 			this.type = 2;
 			//Initialize the graphics for the weapons
+			this.tankShot.type = 2;
 		}
 		else if(this.color = "yellow"){
 			this.type = 3;
 			//Initialize the graphics for the weapons
+			this.tankShot.type = 3;
 		}
 
 		//Load the width and height based on the image and scale
+		this.tankShot.updateProportions();
+
 		this.width = this.image[this.type].width/this.factor;
 		this.height = this.image[this.type].height/this.factor;
 	}
@@ -254,6 +283,12 @@ function Tank(x,y,color,stance){
 				ctx.drawImage(this.image[this.type+4], this.x, this.y, this.width, this.height);
 			}
 		}
+
+		// //Drawing the shot
+		// if(this.shooting){
+		// 	//this.tankShot.draw();
+		// 	this.tankShot.updateShot();
+		// }
 	}
 }
 
@@ -269,6 +304,7 @@ function draw(){
 	clearCanvas();
 	t.draw();
 	t2.draw();
+	t.tankShot.draw();
 }
 
 function update(){
